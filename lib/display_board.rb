0caa95132aa0c7db09c1e55130abacdccc466616
friendly-format-0074 <samuelagasi@gmail.com>
2021-01-11 22:35:15 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,2 +1,15 @@
-# Define display_board that accepts a board and prints
-# out the current state.
+board= [" "," "," "," ","X"," "," "," "," "]
+
+
+def display_board(board)
+  rows= [" "," "," "," "," "]
+  rows[0]=  "#{board[0]} #{"|"} #{board[1]} #{"|"} #{board[2]}"
+  rows[1]=  "-----------"
+  rows[2]=  "#{board[3]} #{"|"} #{board[4]} #{"|"} #{board[5]}"
+  rows[3]=  "-----------"
+  rows[4]=  "#{board[6]} #{"|"} #{board[7]} #{"|"} #{board[8]}"
+  rows
+
+end
+
+puts display_board(board)
